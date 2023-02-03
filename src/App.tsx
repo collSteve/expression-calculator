08aa32eed6fe681dd36c723d92838293c9c86767
Ke Ren <rkglimmer16@gmail.com>
2023-02-03 14:48:37 -0800

    Create demo for elementry oprestions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Constant, Multiply, Add } from './expression-calc/model/expression';
+import { Environment } from './expression-calc/model/environment';
 
 function App() {
+  const env: Environment = new Environment();
+
+  const c2 = new Constant(2);
+  const c3 = new Constant(3);
+  const c5 = new Constant(5);
+
+  const expressions = [
+    new Multiply(c2, new Add(c5, c3)),
+    new Add(c2, new Multiply(c5, c3)),
+    new Add(new Multiply(c5, c3), c2),
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {expressions.map(expr => {
+        return <p>{`${expr} = ${expr.evaluate(env)}`}</p>
+      })}
     </div>
   );
 }
